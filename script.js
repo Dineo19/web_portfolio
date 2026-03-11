7c17a3e8749a1802271a2e86e881d35bb0f3759d
Dineo Matlabyana <matlabyanadineo1@gmail.com>
2026-03-11 12:29:28 +0200

    web update

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({
-        behavior: 'smooth'
-    });
-}
+const form = document.getElementById("contactForm");
+const success = document.getElementById("success");
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+success.innerHTML="✔ Message Sent";
+
+setTimeout(()=>{
+success.innerHTML="";
+form.reset();
+},3000)
+
+})
